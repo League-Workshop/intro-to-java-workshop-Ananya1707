e43fcb43cf07faa28139e548e08e4bf905ea7d28
root <root@d28d9a420a76>
2020-04-21 21:59:49 +0000

    Java Workshop Day 2
    Learned how to make something move and draw thing.

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,6 +8,19 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+		Robot rob = new Robot("mini");
+		
+		//int x = 5;
+		
+		rob.penDown();
+		//rob.penUp();
+		rob.setSpeed(100);
+		
+		for (int i = 0; i < 4; i++) {
+			rob.move(50);
+			rob.turn(90);
+		}
+		
 	
 		
 		
